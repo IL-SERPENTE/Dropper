0320673057924894aef9a4284de572bfa269ee3b
Vialonyx <pro.vialonyx@gmail.com>
2016-12-25 01:03:44 +0100

    Initial

---
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -0,0 +1,16 @@
+package net.samagames.dropper;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * @author Vialonyx
+ */
+
+public class Dropper extends JavaPlugin {
+
+    @Override
+    public void onEnable(){
+
+    }
+
+}
