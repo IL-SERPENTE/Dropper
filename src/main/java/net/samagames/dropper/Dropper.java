6a723a3d4cf3447d05eba8842980b58bf90919d2
Kiliann <pro.vialonyx@gmail.com>
2016-12-26 12:04:07 +0100

    Disabling 'doDaylightCycle' gamerule.

--- src/main/java/net/samagames/dropper/Dropper.java
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -1,6 +1,7 @@
 package net.samagames.dropper;
 
 import net.samagames.api.SamaGamesAPI;
+import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /**
@@ -9,10 +10,16 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class Dropper extends JavaPlugin {
 
+    private World world;
     private DropperGame dropperGame;
 
     @Override
     public void onEnable(){
+
+        this.world = this.getServer().getWorlds().get(0);
+        this.world.setGameRuleValue("doDaylightCycle", "false");
+
+        // -- SamaGames --
         this.dropperGame = new DropperGame("gameCode", "Dropper", "gameDesc", DropperPlayer.class);
         SamaGamesAPI.get().getGameManager().registerGame(this.dropperGame);
     }
