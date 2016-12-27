d96a8487b7a080ac83c0e5c1ca771952171280dd
Vialonyx <pro.vialonyx@gmail.com>
2016-12-27 01:49:44 +0100

    Added level names

--- src/main/java/net/samagames/dropper/level/LevelRegistery.java
+++ src/main/java/net/samagames/dropper/level/LevelRegistery.java
@@ -17,7 +17,7 @@ public class LevelRegistery {
     public final AbstractLevel LEVEL_1;
 
     public LevelRegistery(){
-        this.LEVEL_1 = new AbstractLevel(1, GameLocations.LEVEL1_AREA.locationValue(),
+        this.LEVEL_1 = new AbstractLevel(1, "Rainbow",GameLocations.LEVEL1_AREA.locationValue(),
                 new Location(getWorlds().get(0), 535, 234, -37));
     }
 
