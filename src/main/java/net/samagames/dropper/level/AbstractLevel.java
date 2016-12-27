3b79d2a9cc82cda96e43b43a44b020de7046dff0
Vialonyx <pro.vialonyx@gmail.com>
2016-12-27 01:49:44 +0100

    Added level names

--- src/main/java/net/samagames/dropper/level/AbstractLevel.java
+++ src/main/java/net/samagames/dropper/level/AbstractLevel.java
@@ -19,23 +19,31 @@ public class AbstractLevel {
      */
 
     private int levelNumber;
+    private String levelName;
     private Location levelLocation;
     private Location secretAsLocation;
     private ArmorStand secretAs;
     private List<Player> levelPlayers;
 
-    public AbstractLevel(int levelNumber, Location levelLocation, Location secretAsLocation){
+    public AbstractLevel(int levelNumber, String levelName, Location levelLocation, Location secretAsLocation){
         this.levelNumber = levelNumber;
+        this.levelName = levelName;
         this.levelLocation = levelLocation;
         this.secretAsLocation = secretAsLocation;
         this.levelPlayers = new ArrayList<>();
         this.secretAs = getWorlds().get(0).spawn(this.secretAsLocation, ArmorStand.class);
         this.secretAs.setVisible(false);
         this.secretAs.setGravity(false);
+        this.secretAs.setCustomName("§3§lNiveau §c§l" + this.levelNumber + " §f(§b§o" + this.levelName + "§f)");
+        this.secretAs.setCustomNameVisible(true);
     }
 
     public int getNumber(){
         return this.levelNumber;
+    }
+
+    public String getLevelName(){
+        return this.levelName;
     }
 
     public Location getRelatedLocation(){
