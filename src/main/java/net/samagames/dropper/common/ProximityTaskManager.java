f7d82aa09ec6f09afb6892edeb2c5085ad968ccb
Vialonyx <pro.vialonyx@gmail.com>
2016-12-29 00:36:25 +0100

    Introducing win detection using proximity tasks

--- src/main/java/net/samagames/dropper/common/ProximityTaskManager.java
+++ src/main/java/net/samagames/dropper/common/ProximityTaskManager.java
@@ -3,7 +3,6 @@ package net.samagames.dropper.common;
 import net.samagames.dropper.Dropper;
 import net.samagames.tools.ProximityUtils;
 import org.bukkit.entity.Player;
-import org.bukkit.scheduler.BukkitTask;
 
 /**
  * @author Vialonyx
@@ -11,16 +10,19 @@ import org.bukkit.scheduler.BukkitTask;
 
 public class ProximityTaskManager {
 
-    private BukkitTask proximityLevel1;
-
     private Dropper instance;
     public ProximityTaskManager(Dropper instance){
         this.instance = instance;
 
-        this.proximityLevel1 = ProximityUtils.onNearbyOf(this.instance,
-                this.instance.getLevelManager().LEVEL_1.getSecretAs(),
+        ProximityUtils.onNearbyOf(this.instance,
+                this.instance.getLevelManager().LEVEL_1.getStartSecretAs(),
                 2.0D, 2.0D, 2.0D, Player.class, player -> this.instance.getServer().getScheduler().runTask(instance,
                         () -> this.instance.getLevelManager().joinLevel(player, this.instance.getLevelManager().LEVEL_1)));
+        
+        ProximityUtils.onNearbyOf(this.instance,
+                this.instance.getLevelManager().LEVEL_1.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.instance.getServer().getScheduler().runTask(instance,
+                        () -> this.instance.getLevelManager().setLevelWin(player)));
 
     }
 
