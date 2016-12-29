75aa70588e5354b4f2360d028a710b6d8df5baa6
Vialonyx <pro.vialonyx@gmail.com>
2016-12-29 12:07:05 +0100

    Refactoring

--- src/main/java/net/samagames/dropper/Dropper.java
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -5,14 +5,16 @@ import net.minecraft.server.v1_10_R1.PacketPlayOutTitle;
 import net.minecraft.server.v1_10_R1.PacketPlayOutTitle.EnumTitleAction;
 import net.minecraft.server.v1_10_R1.IChatBaseComponent.ChatSerializer;
 import net.samagames.api.SamaGamesAPI;
-import net.samagames.dropper.common.ProximityTaskManager;
 import net.samagames.dropper.events.PlayerListener;
 import net.samagames.dropper.events.WorldListener;
 import net.samagames.dropper.level.LevelManager;
+import net.samagames.tools.ProximityUtils;
 import org.bukkit.World;
 import org.bukkit.craftbukkit.v1_10_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 /**
  * @author Vialonyx
@@ -23,7 +25,6 @@ public class Dropper extends JavaPlugin {
     private World world;
     private DropperGame dropperGame;
     private LevelManager levelManager;
-    private ProximityTaskManager proximityTaskManager;
     private DropperBoard dropperBoard;
 
     @Override
@@ -34,7 +35,6 @@ public class Dropper extends JavaPlugin {
 
         this.dropperBoard = new DropperBoard(this);
         this.levelManager = new LevelManager(this);
-        this.proximityTaskManager = new ProximityTaskManager(this);
 
         this.getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
         this.getServer().getPluginManager().registerEvents(new WorldListener(this), this);
@@ -65,6 +65,16 @@ public class Dropper extends JavaPlugin {
 	}
     
     /**
+     * Removing all effects and restore initial NightVision effect
+     * @param player the player
+     */
+    
+    public void resetPotionEffects(Player player){
+    	player.getActivePotionEffects().clear();
+        player.addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, 99999, 2, false, false));
+    }
+    
+    /**
      * Get the first world
      * @return world the world
      */
@@ -72,6 +82,11 @@ public class Dropper extends JavaPlugin {
     public World getWorld(){
         return this.world;
     }
+    
+    /**
+     * Get an instance of DropperBoard
+     * @return instance of DropperBoard
+     */
     
     public DropperBoard getDropperBoard(){
     	return this.dropperBoard;
@@ -93,6 +108,94 @@ public class Dropper extends JavaPlugin {
 
     public LevelManager getLevelManager(){
         return this.levelManager;
+    }
+    
+    /**
+     * Start all proximity tasks
+     */
+    
+    public void startProximityTasks(){
+    	
+    	ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_1.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_1)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_1.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_2.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_2)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_2.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_3.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_3)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_3.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_4.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_4)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_4.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_5.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_5)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_5.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_6.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_6)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_6.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_7.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_7)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_7.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_8.getStartSecretAs(),
+                1.0D, 1.0D, 1.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().joinLevel(player, this.getLevelManager().LEVEL_8)));
+        
+        ProximityUtils.onNearbyOf(this,
+                this.getLevelManager().LEVEL_8.getWinSecretAs(),
+                2.0D, 2.0D, 2.0D, Player.class, player -> this.getServer().getScheduler().runTask(this,
+                        () -> this.getLevelManager().setLevelWin(player)));
+    	
     }
 
 }
