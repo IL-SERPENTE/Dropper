e862969f5e3aaa8c2bb2590109819084c4ea1a7a
Vialonyx <pro.vialonyx@gmail.com>
2017-01-14 10:30:53 +0100

    Added items

--- src/main/java/net/samagames/dropper/Dropper.java
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -1,7 +1,12 @@
 package net.samagames.dropper;
 
+import java.util.List;
+
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import com.google.gson.JsonObject;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.api.games.Game;
@@ -11,8 +16,16 @@ public class Dropper extends Game<DropperPlayer> {
 	
 	private DropperMain instance;
 	
+	public final ItemStack ITEM_GAMETYPE_SELECT_FREE;
+	public final ItemStack ITEM_GAMETYPE_SELECT_COMPETITION;
+	public final ItemStack ITEM_ACTUAL_LEAVE;
+	
 	 public Dropper(String gameCodeName, String gameName, String gameDescription, Class<DropperPlayer> gamePlayerClass, DropperMain instance) {
 		 super(gameCodeName, gameName, gameDescription, gamePlayerClass);
+		 
+		 this.ITEM_GAMETYPE_SELECT_FREE = this.stackBuilder("Entrainement", null, Material.DIRT, (byte) 0);
+		 this.ITEM_GAMETYPE_SELECT_COMPETITION = this.stackBuilder("Compétition", null, Material.GRASS, (byte) 0);
+		 this.ITEM_ACTUAL_LEAVE = this.stackBuilder("Quitter le niveau actuel", null, Material.BIRCH_DOOR_ITEM, (byte) 0);
 	 }
 	 
 	 @Override 
@@ -46,6 +59,16 @@ public class Dropper extends Game<DropperPlayer> {
 			  */
 		 }
 	 }
+	 
+	 private ItemStack stackBuilder(String name, List<String> lore, Material material, byte data){ 
+	        org.bukkit.inventory.ItemStack tmpStack = new ItemStack(material, 1, data); 
+	        ItemMeta tmpStackMeta = tmpStack.getItemMeta(); 
+	        tmpStackMeta.setDisplayName(name); 
+	        tmpStackMeta.setLore(lore); 
+	        tmpStack.setItemMeta(tmpStackMeta); 
+	 
+	        return tmpStack; 
+	    } 
 	 
 	 public Location getMapHub(){
 	    JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
