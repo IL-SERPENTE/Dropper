62cfcf7b8bae77a81544960b9f59211006002e9d
Vialonyx <pro.vialonyx@gmail.com>
2017-01-14 10:19:51 +0100

    Getting level data from Json file

--- src/main/java/net/samagames/dropper/level/DropperLevel.java
+++ src/main/java/net/samagames/dropper/level/DropperLevel.java
@@ -1,14 +1,25 @@
 package net.samagames.dropper.level;
 
+import org.bukkit.Location;
+import com.google.gson.JsonObject;
+import net.samagames.api.SamaGamesAPI;
+import net.samagames.tools.LocationUtils;
+
 public class DropperLevel {
 	
 	private int levelID;
 	private String levelName, levelDescription;
+	private Location levelPlayLocation;
 	
 	public DropperLevel(int levelID, String levelName, String levelDescription){
 		this.levelID = levelID;
 		this.levelName = levelName;
 		this.levelDescription = levelDescription;
+		
+		 // Getting level data from Json file
+        JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
+        this.levelPlayLocation = LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("level" + levelID).getAsString());
+		
 	}
 	
 	public int getID(){
@@ -21,6 +32,10 @@ public class DropperLevel {
 	
 	public String getDescription(){
 		return this.levelDescription;
+	}
+	
+	public Location getPlayLocation(){
+		return this.levelPlayLocation;
 	}
 
 }
