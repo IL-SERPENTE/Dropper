4b1770c1e1c6326fa246cc0c1412e04354428ec3
Vialonyx <pro.vialonyx@gmail.com>
2017-01-27 11:49:14 +0100

    Generating secrets ArmorStand on level

--- src/main/java/net/samagames/dropper/level/DropperLevel.java
+++ src/main/java/net/samagames/dropper/level/DropperLevel.java
@@ -1,15 +1,21 @@
 package net.samagames.dropper.level;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import com.google.gson.JsonObject;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.tools.LocationUtils;
+import org.bukkit.World;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 
 public class DropperLevel {
 	
 	private int levelID;
 	private String levelName, levelDescription;
+	private World world;
 	private Location levelPlayLocation;
+	private ArmorStand LevelAs_Start, LevelAs_End;
 	
 	public DropperLevel(int levelID, String levelName, String levelDescription){
 		this.levelID = levelID;
@@ -18,8 +24,18 @@ public class DropperLevel {
 		
 		 // Getting level data from Json file
         JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
-        this.levelPlayLocation = LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("level" + levelID).getAsString());
-		
+        this.world = Bukkit.getWorld(object.get("world-name").getAsString());
+        this.levelPlayLocation = LocationUtils.str2loc(this.world.getName() + ", " + object.get("level" + levelID).getAsString());
+        this.LevelAs_Start = this.armorStandBuilder(LocationUtils.str2loc(this.world.getName() +  ", " + object.get("level" + this.levelID + "-asStart").getAsString()));
+		this.LevelAs_End = this.armorStandBuilder(LocationUtils.str2loc(this.world.getName() +  ", " + object.get("level" + this.levelID + "-asWin").getAsString()));
+
+	}
+
+	private ArmorStand armorStandBuilder(Location spawn){
+		ArmorStand as = (ArmorStand) this.world.spawnEntity(spawn, EntityType.ARMOR_STAND);
+		as.setVisible(false);
+		as.setGravity(false);
+		return as;
 	}
 	
 	public int getID(){
@@ -36,6 +52,14 @@ public class DropperLevel {
 	
 	public Location getPlayLocation(){
 		return this.levelPlayLocation;
+	}
+
+	public ArmorStand getSecretStart(){
+		return this.LevelAs_Start;
+	}
+
+	public ArmorStand getSecretEnd(){
+		return this.LevelAs_End;
 	}
 
 }
