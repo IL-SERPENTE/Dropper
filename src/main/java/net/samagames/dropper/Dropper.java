b979251c524128a4bb4dc11c28abff0b23fb7c51
Vialonyx <pro.vialonyx@gmail.com>
2017-01-29 11:23:56 +0100

    Using Lists & Maps to register levels and game items

--- src/main/java/net/samagames/dropper/Dropper.java
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -1,7 +1,9 @@
 package net.samagames.dropper;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
 import net.samagames.dropper.level.DropperLevel;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -17,39 +19,31 @@ import net.samagames.tools.LocationUtils;
 public class Dropper extends Game<DropperPlayer> {
 	
 	private DropperMain instance;
-	
-	public ItemStack ITEM_GAMETYPE_SELECT_FREE;
-	public ItemStack ITEM_GAMETYPE_SELECT_COMPETITION;
-	public ItemStack ITEM_GAMETYPE_LEAVE;
-	public ItemStack ITEM_ACTUAL_LEAVE;
-
-	private DropperLevel LVL_1;
-	private DropperLevel LVL_2;
-	private DropperLevel LVL_3;
-	private DropperLevel LVL_4;
-	private DropperLevel LVL_5;
-	private DropperLevel LVL_6;
-	private DropperLevel LVL_7;
-	private DropperLevel LVL_8;
+	private Map<Integer, ItemStack> gameItems;
+	private List<DropperLevel> registeredLevels;
 	
 	 public Dropper(String gameCodeName, String gameName, String gameDescription, Class<DropperPlayer> gamePlayerClass, DropperMain instance) {
 		 super(gameCodeName, gameName, gameDescription, gamePlayerClass);
 		 
 		 this.instance = instance;
-		 this.ITEM_GAMETYPE_SELECT_FREE = this.stackBuilder("Entrainement", null, Material.DIRT, (byte) 0);
-		 this.ITEM_GAMETYPE_SELECT_COMPETITION = this.stackBuilder("Compétition", null, Material.GRASS, (byte) 0);
-		 this.ITEM_GAMETYPE_LEAVE = this.stackBuilder("Quitter le mode de jeu actuel", null, Material.BONE, (byte) 0);
-		 this.ITEM_ACTUAL_LEAVE = this.stackBuilder("Quitter le niveau actuel", null, Material.BIRCH_DOOR_ITEM, (byte) 0);
 
-		 // Creating levels
-		 this.LVL_1 = new DropperLevel(1, "Rainbow", "n/a");
-		 this.LVL_2 = new DropperLevel(2, "Isengard", "n/a");
-		 this.LVL_3 = new DropperLevel(3, "Neo", "n/a");
-		 this.LVL_4 = new DropperLevel(4, "Symbols", "n/a");
-		 this.LVL_5 = new DropperLevel(5, "The Three", "n/a");
-		 this.LVL_6 = new DropperLevel(6, "Embryo", "n/a");
-		 this.LVL_7 = new DropperLevel(7, "Brain", "n/a");
-		 this.LVL_8 = new DropperLevel(8, "Dimension Jumper", "n/a");
+		 // Registering items
+		 this.gameItems = new HashMap<>();
+		 this.gameItems.put(0, this.stackBuilder("Entrainement", null, Material.DIRT, (byte) 0));
+		 this.gameItems.put(1, this.stackBuilder("Compétition", null, Material.GRASS, (byte) 0));
+		 this.gameItems.put(2, this.stackBuilder("Quitter le mode de jeu actuel", null, Material.BONE, (byte) 0));
+		 this.gameItems.put(3, this.stackBuilder("Quitter le niveau actuel", null, Material.BIRCH_DOOR_ITEM, (byte) 0));
+
+		 // Registering levels
+		 this.registeredLevels = new ArrayList<>();
+		 this.registeredLevels.add(new DropperLevel(1, "Rainbow", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(2, "Isengard", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(3, "Neo", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(4, "Symbols", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(5, "The Three", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(6, "Embryo", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(7, "Brain", "n/a"));
+		 this.registeredLevels.add(new DropperLevel(8, "Dimension Jumper", "n/a"));
 
 	 }
 	 
@@ -58,12 +52,30 @@ public class Dropper extends Game<DropperPlayer> {
 		 super.handleLogin(player);
 		 player.teleport(this.getMapHub());
 		 player.getInventory().clear();
-		 player.getInventory().setItem(0, this.ITEM_GAMETYPE_SELECT_FREE);
-		 player.getInventory().setItem(1, this.ITEM_GAMETYPE_SELECT_COMPETITION);
+		 player.getInventory().setItem(0, this.getGameItem(0));
+		 player.getInventory().setItem(1, this.getGameItem(1));
 	 }
 	 
 	 public DropperMain getMainInstance(){
 		 return this.instance;
+	 }
+
+	 public ItemStack getGameItem(int ref){
+	 	return this.gameItems.get(ref);
+	 }
+
+	public Location getMapHub(){
+		JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
+		return LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("map-hub").getAsString());
+	}
+
+	public Location getMapLevelHub(){
+		JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
+		return LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("level-hub").getAsString());
+	}
+
+	 public DropperLevel getDropperLevel(int ref){
+	 	return this.registeredLevels.get(ref);
 	 }
 	 
 	 public void usualGameTypeUpdate(Player player, GameType newGameType){
@@ -77,7 +89,7 @@ public class Dropper extends Game<DropperPlayer> {
 		 if(dpPlayer.getGameType().equals(GameType.FREE)){
 			 player.teleport(this.getMapLevelHub());
 			 player.getInventory().clear();
-			 player.getInventory().setItem(0, this.ITEM_GAMETYPE_LEAVE);
+			 player.getInventory().setItem(0, this.getGameItem(2));
 
 		 } else if(dpPlayer.getGameType().equals(GameType.COMPETITION)){
 			 /*
@@ -115,8 +127,8 @@ public class Dropper extends Game<DropperPlayer> {
 		 dpPlayer.updatePlayerGameType(GameType.UNSELECTED);
 		 dpPlayer.updateCurrentLevel(null);
 		 player.getInventory().clear();
-		 player.getInventory().setItem(0, this.ITEM_GAMETYPE_SELECT_FREE);
-		 player.getInventory().setItem(1, this.ITEM_GAMETYPE_SELECT_COMPETITION);
+		 player.getInventory().setItem(0, this.getGameItem(0));
+		 player.getInventory().setItem(1, this.getGameItem(1));
 		 
 	 }
 	 
@@ -128,16 +140,6 @@ public class Dropper extends Game<DropperPlayer> {
 	        tmpStack.setItemMeta(tmpStackMeta); 
 	 
 	        return tmpStack; 
-	    } 
-	 
-	 public Location getMapHub(){
-	    JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
-	    return LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("map-hub").getAsString());
-	 }
-	 
-	 public Location getMapLevelHub(){
-	    JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
-	    return LocationUtils.str2loc(object.get("world-name").getAsString() + ", " + object.get("level-hub").getAsString());
-	 }
+	    }
 	
 }
