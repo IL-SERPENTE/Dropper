a7ccad9c1544dcab98cb01b0804b50b374214195
Vialonyx <pro.vialonyx@gmail.com>
2017-02-21 23:40:14 +0100

    Disabling ability to place blocks

--- src/main/java/net/samagames/dropper/events/listeners/PlayerEventsListener.java
+++ src/main/java/net/samagames/dropper/events/listeners/PlayerEventsListener.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
@@ -51,6 +52,11 @@ public class PlayerEventsListener implements Listener {
             }
             
 		}
+	}
+
+	@EventHandler
+	public void onBlockPlace(BlockPlaceEvent event){
+		event.setCancelled(true);
 	}
 
 	@EventHandler
