e31a08da48339bb8a3a23e7d29eb4a911257d583
Vialonyx <pro.vialonyx@gmail.com>
2017-02-23 00:49:29 +0100

    Set value of gamerule randomTickSpeed to 0

--- src/main/java/net/samagames/dropper/DropperMain.java
+++ src/main/java/net/samagames/dropper/DropperMain.java
@@ -5,6 +5,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 import net.samagames.api.SamaGamesAPI;
 import net.samagames.dropper.events.listeners.PlayerEventsListener;
 
+import static org.bukkit.Bukkit.getWorlds;
+
 public class DropperMain extends JavaPlugin {
 
 	/**
@@ -20,10 +22,13 @@ public class DropperMain extends JavaPlugin {
     	this.game = new Dropper("gameCode", "Dropper", "The Dropper", DropperPlayer.class, this);
     	this.getServer().getPluginManager().registerEvents(new PlayerEventsListener(this.game), this);
     	this.getServer().getPluginManager().registerEvents(new LevelActionsListener(this.game), this);
+    	getWorlds().get(0).setGameRuleValue("randomTickSpeed", "0");
 		SamaGamesAPI.get().getGameManager().setFreeMode(true);
     	SamaGamesAPI.get().getGameManager().registerGame(this.game);
     	
     }
+
+
 
 	/**
 	 * Get the internal representation of the game.
