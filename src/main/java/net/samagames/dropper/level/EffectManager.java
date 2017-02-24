e388cbc7ebba0578511a18ab0d428a7067d9546c
Vialonyx <pro.vialonyx@gmail.com>
2017-02-24 22:20:44 +0100

    Potions effect are now permanent

--- src/main/java/net/samagames/dropper/level/EffectManager.java
+++ src/main/java/net/samagames/dropper/level/EffectManager.java
@@ -27,18 +27,18 @@ public class EffectManager {
     public void addEffectsForLevel(Player player, DropperLevel level){
 
         if(level.getID() == 8){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 200, 4));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 600, 2));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 600, 1));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.DAMAGE_RESISTANCE, 999999, 4));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999, 2));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 1));
         } else if (level.getID() == 12){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 9600, 4));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 4));
         } else if(level.getID() == 13) {
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 4000, 4));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 6100, 4));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 999999, 4));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 4));
         } else if(level.getID() == 14){
-            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 10000, 3));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 10000, 0));
-            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 10000, 0));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 3));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.JUMP, 999999, 0));
+            player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 999999, 0));
         } else if(level.getID() == 16){
             player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, 999999, 1));
         }
