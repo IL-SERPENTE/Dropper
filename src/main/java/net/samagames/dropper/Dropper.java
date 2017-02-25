a5653c608c7a0a0c4b6adae9f47a7c92240e7f13
Werter <shai.halimi@gmail.com>
2017-02-25 01:30:15 +0100

    Add a tutorial in the first level

--- src/main/java/net/samagames/dropper/Dropper.java
+++ src/main/java/net/samagames/dropper/Dropper.java
@@ -6,15 +6,14 @@ import net.samagames.dropper.level.DropperLevel;
 import net.samagames.dropper.level.EffectManager;
 import net.samagames.dropper.level.LevelCooldown;
 import net.samagames.tools.ProximityUtils;
+import net.samagames.tools.Rainbow;
 import net.samagames.tools.Titles;
 import net.samagames.tools.chat.ActionBarAPI;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import com.google.gson.JsonObject;
@@ -218,20 +217,64 @@ public class Dropper extends Game<DropperPlayer> {
 	 */
 
 	public void usualLevelJoin(Player player, int levelRef) {
-		DropperPlayer dpPlayer = this.getPlayer(player.getUniqueId());
-		DropperLevel level = this.getDropperLevel(levelRef);
-		// Managing player inventory.
-		player.getInventory().clear();
-		player.getInventory().setItem(4, this.ITEM_QUIT_GAME);
-		// Updating current level of player.
-		dpPlayer.updateCurrentLevel(level);
-		// Sending title with level's name & his description.
-		Titles.sendTitle(player, 30, 70, 30, "" + ChatColor.YELLOW + ChatColor.BOLD + level.getName(), "" + ChatColor.RED + ChatColor.ITALIC + level.getDescription());
-		// Starting cooldown if he does not have anyone started before.
-		if(! dpPlayer.hasActiveCooldown()){
-			new LevelCooldown(this, player, level).runTaskTimer(this.instance, 0L, 20L);
-		}
-	}
+        DropperPlayer dpPlayer = this.getPlayer(player.getUniqueId());
+        DropperLevel level = this.getDropperLevel(levelRef);
+        if (level.getID() == 1) {
+            BukkitRunnable bukkitRunnable = new BukkitRunnable() {
+                int step = 0;
+                JsonObject object = SamaGamesAPI.get().getGameManager().getGameProperties().getConfigs();
+                @Override
+                public void run() {
+                        if (step == 0) {
+                            player.setGameMode(GameMode.SPECTATOR);
+                            dpPlayer.setNeutralized(true);
+                            player.sendTitle("Bienvenue", "Sur la map The Dropper");
+                            Location loc = LocationUtils.str2loc(getWorlds().get(0).getName() + ", " + object.get("step-1").getAsString());
+                            player.teleport(loc);
+                        } else if (step == 1) {
+                            player.sendTitle("Les blocks d'eau representent l'arrivé", "Il faut rentrer dedans");
+                            Location loc = LocationUtils.str2loc(getWorlds().get(0).getName() + ", " + object.get("step-2").getAsString());
+                            player.teleport(loc);
+                        } else if (step == 2) {
+                            player.sendTitle("Les citrouilles vous permettent de retrouvez votre chemin", "");
+						    Location locPumkin = LocationUtils.str2loc(getWorlds().get(0).getName() + ", " + object.get("tutorialBlock").getAsString());
+						    player.getPlayer().sendBlockChange(locPumkin,Material.PUMPKIN,(byte) 0);
+                        } else if (step == 4) {
+                            player.sendTitle("Les toiles d'araignée permmetent d'atterir en toute sécurité", "");
+                            Location locCobWeb = LocationUtils.str2loc(getWorlds().get(0).getName() + ", " + object.get("tutorialBlock").getAsString());
+                            player.getPlayer().sendBlockChange(locCobWeb, Material.WEB, (byte) 0);
+                        } else if (step == 5) {
+                            player.setGameMode(GameMode.ADVENTURE);
+                            player.sendTitle("Maintenant a toi de jouer", "");
+                            Location loc = LocationUtils.str2loc(getWorlds().get(0).getName() + ", " + object.get("tutorialBlock").getAsString());
+                            player.getPlayer().sendBlockChange(loc, Material.AIR, (byte) 0);
+                            player.teleport(getSpawn());
+                            dpPlayer.setNeutralized(false);
+                            usualStartLevel(dpPlayer,player,level);
+                            this.cancel();
+                        }
+                        step++;
+                }
+            };
+            bukkitRunnable.runTaskTimer(this.getInstance(), 20, 100);
+        } else {
+            usualStartLevel(dpPlayer,player,level);
+        }
+    }
+
+    public void usualStartLevel(DropperPlayer dpPlayer, Player player,DropperLevel level){
+        // Managing player inventory.
+        player.getInventory().clear();
+        player.getInventory().setItem(4, this.ITEM_QUIT_GAME);
+        // Updating current level of player.
+        dpPlayer.updateCurrentLevel(level);
+        // Sending title with level's name & his description.
+        Titles.sendTitle(player, 30, 70, 30, "" + ChatColor.YELLOW + ChatColor.BOLD + level.getName(), "" + ChatColor.RED + ChatColor.ITALIC + level.getDescription());
+        // Starting cooldown if he does not have anyone started before.
+        if (!dpPlayer.hasActiveCooldown()) {
+            new LevelCooldown(this, player, level).runTaskTimer(this.instance, 0L, 20L);
+        }
+    }
 	/**
 	 * This is the entry point of the level-leaving process.
 	 * @param player the player.
